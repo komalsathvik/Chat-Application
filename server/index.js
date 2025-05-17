d2997abc17a554f45ebae356087cea2a0f1008dd
komalsathvik <mksathvik03@gmail.com>
2025-05-17 23:33:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server=http.createServer(app);
 const io=new Server(server,{
   cors:{
-    origin:"http://localhost:5173",
+    origin:"chat-application-t4vi-l0zhbjdor-komalsathviks-projects.vercel.app",
     methods:['GET','POST'],
   }
 });
